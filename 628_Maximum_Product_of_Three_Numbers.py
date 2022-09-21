7ea879bc8d2914fa0086d91bc564aae5262f9faf
toaydiner <ybgirgin3@gmail.com>
2022-09-21 10:51:48 +0300

    628-Leetcode

---
+++ 628_Maximum_Product_of_Three_Numbers.py
@@ -0,0 +1,11 @@
+import heapq
+class Solution:
+    def maximumProduct(self, nums: list[int]) -> int:
+        nums.sort()
+        print(nums)
+        return max(nums[-1] * nums[-2] * nums[-3], nums[0] * nums[1] * nums[-1])
+             
+nums_list = [4,5,-1,-2,-3,-4]
+
+a = Solution()
+print(a.maximumProduct(nums_list))
